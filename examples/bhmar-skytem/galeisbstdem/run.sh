2fb60f02647d81d5d67f103139a4d7636a7202da
Martin Panzner <mp@emrld.no>
2023-02-10 10:32:02 +0100

    chaned to run with openMP without mpirun

--- examples/bhmar-skytem/galeisbstdem/run.sh
+++ examples/bhmar-skytem/galeisbstdem/run.sh
@@ -4,8 +4,8 @@
 #galeisbstdem.exe galeisbstdem.con
 
 #Use 4 MPI processes
-mpirun -np 4 galeisbstdem.exe galeisbstdem.con
+#mpirun -np 4 galeisbstdem.exe galeisbstdem.con
 
 #Use 4 OpenMP threads
-#galeisbstdem.exe galeisbstdem.con 4
+galeisbstdem.exe galeisbstdem.con 4
 
